2efa8fd1536ec0f5bd13864644649723e4c4b368
Saif Hossain <atonlymesaif@gmail.com>
2021-07-25 14:01:05 +0600

    status code updated

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.post('/users', (req, res) => {
 	user
 		.save()
 		.then(() => {
-			res.send(user);
+			res.status(201).send(user);
 		})
 		.catch((e) => {
 			res.status(400).send(e);
@@ -28,7 +28,7 @@ app.post('/tasks', (req, res) => {
 	task
 		.save()
 		.then(() => {
-			res.send(task);
+			res.status(201).send(task);
 		})
 		.catch((error) => {
 			res.status(400).send(error);
