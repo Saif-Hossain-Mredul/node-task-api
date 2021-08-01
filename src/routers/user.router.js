938383da5a09e4c80bb11bda98427f0ac929611b
Saif Hossain <atonlymesaif@gmail.com>
2021-08-01 13:55:54 +0600

    image serving endpoint added

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -174,4 +174,17 @@ userRouter.delete('/users/me/avatar', auth, async (req, res) => {
 	}
 });
 
+userRouter.get('/users/:id/avatar', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+
+		if (!user || !user.avatar) throw new Error();
+
+		// res.set('Content-Type', 'image/jpg')
+		res.send(user.avatar);
+	} catch (err) {
+		res.status(404).send();
+	}
+});
+
 module.exports = userRouter;
