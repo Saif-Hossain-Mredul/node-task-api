d3fd2bb4fd1b6667ffe8eedd44f7063d9e25ad67
Saif Hossain <atonlymesaif@gmail.com>
2021-08-22 15:56:15 +0600

    unused file removed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb://127.0.0.1:27017/task-manager-api';
 
-const connection = mongoose.connect(mongoURI, {
+mongoose.connect(mongoURI, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true,
